f1e59538ccbc3ef9d22ca2e5560f79248131281c
sarascript <correasg.sara@gmail.com>
2019-11-22 13:07:38 +0100

    Retrofit

--- app/src/main/java/es/sarascript/examenandroid/MainActivity.kt
+++ app/src/main/java/es/sarascript/examenandroid/MainActivity.kt
@@ -13,6 +13,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,31 +23,48 @@ class MainActivity : AppCompatActivity() {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main)
 
-            val questionList = (1..5).map {
-                Question(
-                    title = "Question $it",
-                    answer1 = "Answer $it",
-                    answer2 = "Answer $it",
-                    answer3 = "Answer $it",
-                    answer4 = "Answer $it"
-                )
+            val service = RetrofitFactory.makeRetrofitService()
+            CoroutineScope(Dispatchers.IO).launch {
+                val response = service.getPosts()
+                withContext(Dispatchers.Main) {
+                    try {
+                        if (response.isSuccessful) {
+                            response.body();
+                            //Do something with response e.g show to the UI.
+                            val questionList = (1..5).map {
+                                Question(
+                                    title = response.body(results[$it].question),
+                                    answer1 = response.body(results[$it].correct_answer),
+                                    answer2 = response.body(results[$it].incorrect_answers[0]),
+                                    answer3 = response.body(results[$it].incorrect_answers[1]),
+                                    answer4 = response.body(results[$it].incorrect_answers[2])
+                                )
+                            }
+                        } else {
+                            toast("Error: ${response.code()}")
+                        }
+                    } catch (e: HttpException) {
+                        toast("Exception ${e.message}")
+                    } catch (e: Throwable) {
+                        toast("Ooops: Something else went wrong")
+                    }
+                }
             }
-
 
             question_list.layoutManager = LinearLayoutManager(this)
             question_list.adapter = QuestionAdapter(questionList)
             question_list.setHasFixedSize(true)
 
-            val questionsDao = DatabaseFactory.get(this.applicationContext).questionsDao()
-            CoroutineScope(Dispatchers.IO).launch {
-                val question = Question(title = "Question 1", answer1 = "Answer 1", answer2 = "Answer 2", answer3 = "Answer 3", answer4 = "Answer 4")
-                questionsDao.insertAll(question)
+            //val questionsDao = DatabaseFactory.get(this.applicationContext).questionsDao()
+            //CoroutineScope(Dispatchers.IO).launch {
+              //  val question = Question(title = "Question 1", answer1 = "Answer 1", answer2 = "Answer 2", answer3 = "Answer 3", answer4 = "Answer 4")
+                //questionsDao.insertAll(question)
 
-                val questions = questionsDao.getAll()
-                withContext(Dispatchers.Main) {
-                    Log.e("MainActivity", questions.toString())
-                }
-            }
+                //val questions = questionsDao.getAll()
+                //withContext(Dispatchers.Main) {
+                  //  Log.e("MainActivity", questions.toString())
+              //  }
+            //}
 
         }
     }
--- app/src/main/java/es/sarascript/examenandroid/Question.kt
+++ app/src/main/java/es/sarascript/examenandroid/Question.kt
@@ -3,12 +3,13 @@ package es.sarascript.examenandroid
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity (tableName = "questions")
+
 data class Question(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0L,
+
     val title: String,
     val answer1: String,
     val answer2: String,
     val answer3: String,
     val answer4: String
+
 )
